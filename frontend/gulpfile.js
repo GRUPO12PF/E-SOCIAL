5904ef566234c715f871537cae5e5303ceb7ac1c
YonY18 <yonap80@gmail.com>
2022-06-07 15:39:42 -0300

    Actualizacion gulp/sass

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,37 +1,21 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch } = require('gulp');
 
-// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
-//const autoprefixer = require('autoprefixer');
-//const cssnano = require('cssnano');
-//const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
-
-
 
 function css( done ) {
     src('./src/assets/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
         .pipe(sourcemaps.init())
         .pipe( plumber())
         .pipe( sass() ) // Compilarlo
-        //.pipe( postcss([ autoprefixer(), cssnano() ]) )
-        //.pipe(sourcemaps.write('.'))
         .pipe( dest('./src/assets/build/css') ) // Almacenarla en el disco duro
     done();
 }
-
-
 function dev( done ) {
     watch('./src/assets/scss/**/*.scss', css);
-    //watch('src/js/**/*.js', javascript);
     done();
 }
 
-
 exports.css = css;
-//exports.js = javascript;
-//exports.imagenes = imagenes;
-//exports.versionWebp = versionWebp;
-//exports.versionAvif = versionAvif;
 exports.dev = dev ;
