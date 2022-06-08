9a10bba06275a0e37c447d38cca3268ccdf05099
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-08 17:40:09 -0500

    creacion componente homeout

---
+++ frontend/src/components/Homeout/Homeout.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import Modal from "react-modal";
+import { autenticarUser } from "../../redux/actions/actionUser";
+//import s from './Homeout.module.css';
+
+
+Modal.setAppElement("#root");
+
+export default function Homeout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return;
+    }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    let usuarioA = dispatch(autenticarUser(config));
+    usuarioA ? navigate("/home") : null;
+  }, []);
+console.log("prueba")
+  return (
+    <div>
+      {/* <img className={s.image} src={image} alt='' /> */}
+      <div>
+        <Link to="/login">
+          <button>LOGIN</button>
+        </Link>
+        <Link to="/registrar">
+          <button>REGISTER</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
