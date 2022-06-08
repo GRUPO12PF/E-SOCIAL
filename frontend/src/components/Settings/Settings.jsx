10229af40c6b637df0b7baa9fae258b5bc24ce10
YonY18 <yonap80@gmail.com>
2022-06-08 14:42:34 -0300

    rutas nuevas Usuarios, cambio Imagen

---
+++ frontend/src/components/Settings/Settings.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { cambiarImagen, usuarioActual } from "../../redux/actions/actionUser";
+import { useDispatch, useSelector } from "react-redux";
+import profile from "../../assets/images/avatar.png";
+
+export default function Settings() {
+  const dispatch = useDispatch();
+  const usuarioAct = useSelector((state) => state.usuarioActual);
+
+  useEffect(() => {
+    dispatch(usuarioActual());
+  }),
+    [];
+
+  function handleFileImage(image) {
+    dispatch(cambiarImagen(image));
+  }
+
+  return (
+    <div>
+      <div>
+      <div>
+            <label>Imagen</label>
+            <br></br>
+            <input
+              onChange={(e) => handleFileImage(e.target.files[0])}
+              type="file"
+              name="imageFile"
+              accept="image/jpeg, image/png"
+              autoComplete='off' />
+            <br></br>
+            <span>Extenciones Soportadas: jpg/jpeg/png</span>
+            <br></br>
+            <img src={usuarioAct.image.url || profile} alt='No Img' />
+          </div>
+        <div>
+          <Link to="/update-password">
+            <button>Change password</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
