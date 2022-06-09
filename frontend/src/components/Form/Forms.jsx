84b79b03936953f1d6d833eeacac841cd7e5269d
Matias Yosia <matiiyosia@gmail.com>
2022-06-09 15:12:41 -0300

    cambios en el formulario y un poco de estilos

--- frontend/src/components/Form/Forms.jsx
+++ frontend/src/components/Form/Forms.jsx
@@ -4,20 +4,21 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { postCreate } from '../../redux/actions/postProducts';
 import NavBar from '../NavBar/NavBar';
+import s from '../Form/Form.module.css'
 
 const Forms = () => {
     let navigate = useNavigate()
     const dispatch = useDispatch()
 
 
-
-    const form = useRef();
+  
     return (
-        <div>
+        <div className={s.formFondo}>
+            <div>
             <div>
                 <NavBar />
             </div>
-            <Formik
+            <Formik 
                 initialValues={{
                     name: '',
                     description: '',
@@ -27,23 +28,23 @@ const Forms = () => {
                     colection: ''
 
                 }}
-                validate={(valores) => {
+                validate={(values) => {
                     let errors = {}
 
-                    if (!valores.name) {
+                    if (!values.name) {
                         errors.name = 'required field'
-                    } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)) {
+                    } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name)) {
                         errors.name = 'name only'
                     }
-                    if (!valores.description) {
+                    if (!values.description) {
                         errors.description = 'required field'
-                    } else if (!valores.price) {
+                    } else if (!values.price) {
                         errors.price = 'required'
-                    } else if (!valores.image) {
+                    } else if (!values.image) {
                         errors.image = 'required field'
-                    } else if (!valores.ranking) {
+                    } else if (!values.ranking) {
                         errors.ranking = 'required field'
-                    } else if (!valores.colection) {
+                    } else if (!values.colection) {
                         errors.colection = 'required field'
                     }
 
@@ -59,67 +60,80 @@ const Forms = () => {
                 }}
             >
 
-                {({ errors }) => (<Form ref={form} action=""  >
+                {({ errors,handleSubmit}) => (<Form action="" onSubmit={handleSubmit} className={s.formik} >
 
-
-
-                    <label htmlFor="">Name</label>
+                    <div className={s.form}>
+                        
+                    <label htmlFor="" className={s.label} >Name</label>
                     <div>
                         <Field
+                            className={s.input}
                             type="text"
                             name="name"
                             id="name"
+                         
                         />
                         <ErrorMessage name='name' component={() => (<p>{errors.name}</p>)} />
                     </div>
-                    <label htmlFor="">Colection</label>
+                    <label htmlFor="" className={s.label} >Colection</label>
                     <div>
                         <Field
+                            className={s.input}
                             type="text"
                             name="colection"
                             id="colection"
+                           
                         />
                         <ErrorMessage name='colection' component={() => (<p>{errors.colection}</p>)} />
                     </div>
 
-                    <label htmlFor="">Price</label>
+                    <label htmlFor="" className={s.label} >Price</label>
                     <div>
                         <Field
+                            className={s.input}
                             type="text"
                             name="price"
                             id="price"
+                         
                         />
 
                         <ErrorMessage name='price' component={() => (<p >{errors.price}</p>)} />
                     </div>
-                    <label htmlFor="">Description</label>
-                    <div>
-                        <Field
-                            type="text"
-                            name="description"
-                            id="description"
-                            as="textarea"
-                        />
-                        <ErrorMessage name='description' component={() => (<p>{errors.description}</p>)} />
 
-                    </div>
-                    <label htmlFor="">Image</label>
+                    <label htmlFor="" className={s.label} >Image</label>
                     <div>
                         <Field
+                            className={s.input}
                             type="text"
                             name="image"
                             id="image"
-                            as="textarea"
+                            
                         />
                         <ErrorMessage name='image' component={() => (<p>{errors.image}</p>)} />
 
                     </div>
-                    <label htmlFor="">Ranking</label>
+                    <label htmlFor="" className={s.label} >Description</label>
                     <div>
                         <Field
+                            className={s.textarea}
+                            type="text"
+                            name="description"
+                            id="description"
+                            as="textarea"
+                      
+                        />
+                        <ErrorMessage name='description' component={() => (<p>{errors.description}</p>)} />
+
+                    </div>
+                    
+                    <label htmlFor="" className={s.label} >Ranking</label>
+                    <div className={s.range}>
+                        <Field
+                            
                             type="range"
                             name="ranking"
                             id="ranking"
+                            
 
                         />
                         <ErrorMessage name='ranking' component={() => (<p>{errors.ranking}</p>)} />
@@ -127,11 +141,13 @@ const Forms = () => {
                     </div>
                     <button type="submit">Send Message</button>
 
+                    </div>
+
                 </Form>)}
             </Formik>
             <br />
             <Link to="/home">Back</Link>
-
+            </div>
         </div>
     )
 }
