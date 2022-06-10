c818ca4936dc6c22f1ff30150c73517f81cfb1a4
CEECISOOL <cbritez6@gmail.com>
2022-06-10 00:09:38 -0300

    arreglo del paginado de filtros en el back

--- backend/controllers/booksControllers.js
+++ backend/controllers/booksControllers.js
@@ -8,7 +8,7 @@ const obtenerBooks = async (req, res) => {
     if (req.query.name) {
       const { name } = req.query
       const bookQuery = await Book.find({ 'nombre': { $regex: `^.*${name}.*`, $options: 'i' } }, projection)
-      
+
       if (bookQuery.length) {
         res.json(bookQuery)
       } else {
@@ -17,38 +17,52 @@ const obtenerBooks = async (req, res) => {
       }
 
       // booksByCategory
-    } else if (req.query.category) {
-      const { category } = req.query
-      const categoryQuery = await Book.find({ category: { $in: [`${ category }`] } }, '-createdAt -updatedAt -__v -avaliable')
-      if (categoryQuery.length) {
-        res.json(categoryQuery)
-      } else {
-        const error = new Error('No se encontraron libros en esa categoría.')
-        res.status(404).json({ msg: error.message })
-      }
-
     } else {
       // getAllBooks      
+      const { category } = req.query
       const limit = req.query.limit || 3
-      const page = Math.ceil(req.query.page) || 1
-      const books = await Book.paginate({}, { projection, limit, page })
-      const totalBooks = books.docs
-
-      res.json(totalBooks)
+      const page = req.query.page || 1
+      if (category) {
+        const categoryResponse = await Book.paginate({ category: { $in: [`${category}`] } }, { projection, limit, page })
+        let response = categoryResponse.docs
+        res.json(response)
+      } else {
+        const books = await Book.paginate({}, { projection, limit, page })
+        const allBooks = books.docs
+        res.json(allBooks)
+      }
     }
   } catch (error) {
     console.log(error)
   }
 }
 
-const obternerTodosLosLibros = async (req, res) =>{
+/*const obternerTodosLosLibros = async (req, res) =>{
     const limit = req.query.limit || 3
     const page = Math.ceil(req.query.page) || 1
     const books = await Book.paginate({}, { projection, limit, page })
     const allBooks = books.totalDocs 
 
     res.json(allBooks)
+}*/
+
+const obternerTodosLosLibros = async (req, res) => {
+  const { category } = req.query
+  const limit = req.query.limit || 3
+  const page = req.query.page || 1
+  if (category) {
+    const categoryResponse = await Book.paginate({ category: { $in: [`${category}`] } }, { projection, limit, page })
+    let response = categoryResponse.totalDocs
+    res.json(response)
+  } else {
+    const books = await Book.paginate({}, { projection, limit, page })
+    const allBooks = books.totalDocs
+    res.json(allBooks)
+  }
+
+
 }
+
 
 const nuevoBook = async (req, res) => {
   const book = new Book(req.body)
@@ -67,7 +81,7 @@ const detailBook = async (req, res) => {
     const { id } = req.params
     const book = await Book.findById(id, projection)
 
-    
+
 
     if (!book) {
       const error = new Error('No se encontró el libro.')
