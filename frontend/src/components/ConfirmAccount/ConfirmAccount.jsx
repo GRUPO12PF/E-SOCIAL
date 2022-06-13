66539aec1ff05f2fec1c57ac2fdb5ec4d2c054de
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-13 09:32:21 -0500

    estilos componente confirm account

--- frontend/src/components/ConfirmAccount/ConfirmAccount.jsx
+++ frontend/src/components/ConfirmAccount/ConfirmAccount.jsx
@@ -2,25 +2,29 @@ import React, { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { validateUser } from "../../redux/actions/actionUser.js";
+import s from "./ConfirmAccount.module.css";
 
 export default function ConfirmarCuenta() {
   const dispatch = useDispatch();
   const respuesta = useSelector((state) => state.confirmacion);
   const params = useParams();
   const { id } = params;
+  
 
   useEffect(() => {
     dispatch(validateUser(id));
   }, []);
 
   return (
-    <div>
-      <h2>
+    <div className={s.contConfirm}>
+      <h2 className={s.title}>
         Welcome to <span>Books</span> Market
+        <br/>
       </h2>
-      <div>{respuesta.msg}</div>
+      <div className={s.response}>{respuesta.msg}</div>
+      <br/>
       <Link to="/">
-        <button>Go back home</button>
+        <button className={s.btn}>Go back home</button>
       </Link>
     </div>
   );
