b8125f973f26ab8ef44bbe659721c814ef494725
Nekrocow <nekrocow@gmail.com>
2022-06-16 18:15:38 -0300

    stripe payments not confirmed yet

--- backend/controllers/paymentControllers.js
+++ backend/controllers/paymentControllers.js
@@ -1,33 +1,25 @@
-import stripeImport from 'stripe';
+import stripeImport from 'stripe'
 import getOrderAmount from '../helpers/getOrderAmount.js'
 const stripe = stripeImport(process.env.STRIPE_SECRET_KEY)
 
 const paymentIntent = async (req, res) => {
-    const books = req.body;
-    console.log("🚀 ~ file: paymentControllers.js ~ line 7 ~ paymentIntent ~ books", books.pm)
-    console.log("🚀 ~ file: paymentControllers.js ~ line 7 ~ paymentIntent ~ req.body", req.body)
+  const books = req.body
 
-    // console.log("a ver qué trae los body", req.body)
-    // console.log("toy en controladores de pago", )
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: await getOrderAmount(books),
+    currency: "ars",
+    payment_method: books.pm,
+    automatic_payment_methods: {
+      enabled: true,
+    }
+  })
+  // stripe.confirmCardPayment(paymentIntent.client_secret, books.pm)
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: await getOrderAmount(books),
-      currency: "ars",
-      payment_method: books.pm,
-      automatic_payment_methods: {
-        enabled: true,
-      }
-      // confirm: true,
-      // automatic_payment_methods: {
-      //   enabled: true,
-      // },
-    });
-  
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  };
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  })
+}
 
 export {
-    paymentIntent
+  paymentIntent
 }
