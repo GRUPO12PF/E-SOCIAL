cbbce3f29809ba3e99ae25db253997db7830673a
Nekrocow <nekrocow@gmail.com>
2022-06-16 09:47:46 -0300

    fixeando

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import style from './Book.module.css'
 
-function Book({nombre,image, price}) {
-    const { id } = useParams()
-    const handleBuy = () => {
-            dispatch(buyBook(id))
-        }
-    return (
-        <div className={style.bookItem}>
-            
-            <div>
-                <h1 className= {style.nombre}>{nombre}</h1>
-                <img
-                    className={style.book}
-                    src={image} 
-                    alt='Img not found'
-                    onError={(e)=>e.target.setAttribute('src','https://pbs.twimg.com/profile_images/1611903252/Books-Icon120x120_400x400.jpg')} />
-                    <h2 className={style.nombre}>Precio: {price}</h2>
-                    <Link to= '/checkout'>
-                        <button onClick={(e) => handleBuy(e)} >BUY NOW</button>
-                    </Link>
-            </div>
-        </div>
-    )
+function Book({ nombre, image, price }) {
+  const { id } = useParams()
+  const handleBuy = () => {
+    dispatch(buyBook(id))
+  }
+  return (
+    <div className={style.bookItem}>
+
+      <div>
+        <h1 className={style.nombre}>{nombre}</h1>
+        <img
+          className={style.book}
+          src={image}
+          alt='Img not found'
+          onError={(e) => e.target.setAttribute('src', 'https://pbs.twimg.com/profile_images/1611903252/Books-Icon120x120_400x400.jpg')} />
+        <h2 className={style.nombre}>Precio: {price}</h2>
+        {/* <Link to='/checkout'>
+          <button onClick={(e) => handleBuy(e)}>BUY NOW</button>
+        </Link> */}
+      </div>
+    </div>
+  )
 }
 
 export default Book
--- frontend/src/components/Details/Details.jsx
+++ frontend/src/components/Details/Details.jsx
@@ -1,51 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from "react-router";
-import { useParams } from 'react-router-dom'
+import { useNavigate } from "react-router"
+import { Link, useParams } from 'react-router-dom'
 import { detailsBook } from '../../redux/actions/detailsBooks'
-import NavBar from "../NavBar/NavBar";
-import s from './Details.module.css';
-import {deleteBook} from '../../redux/actions/actionBooks'
+import NavBar from "../NavBar/NavBar"
+import s from './Details.module.css'
+import { deleteBook } from '../../redux/actions/actionBooks'
 import NotFoundGral from '../NotFound/NotFoundGral'
-import Loading from '../Loading/Loading';
-import {buyBook} from '../../redux/actions/actionBuy'
+import Loading from '../Loading/Loading'
+
+import { buyBook } from '../../redux/actions/actionBuy'
 
 const Details = () => {
-  const [loading, setLoading] = useState(true);
-  const token = localStorage.getItem("token");
+  const [loading, setLoading] = useState(true)
+  const token = localStorage.getItem("token")
   const { id } = useParams()
   const dispatch = useDispatch()
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const detail = useSelector((state) => state.detail)
   // console.log(detail)
 
-  if(Object.keys(detail).length > 0 && loading){
-    setLoading(false);
-}
+  if (Object.keys(detail).length > 0 && loading) {
+    setLoading(false)
+  }
 
   useEffect(() => {
-    
     dispatch(detailsBook(id))
-        
   }, [dispatch])
 
   if (!token) {
-    navigate("/");
+    navigate("/")
   }
 
-  const handleBuy = () => {
-        e.preventDefault()
-        dispatch(buyBook(id))
-      }
+  // const handleBuy = () => {
+  //   e.preventDefault()
+  //   dispatch(buyBook(id))
+  // }
+
   function handleDeleteBook(e) {
     e.preventDefault()
     dispatch(deleteBook(id))
-      navigate('/home')
-      window.location.reload();
+    navigate('/home')
+    window.location.reload()
   }
   function handleUpdateBook(e) {
     e.preventDefault()
-     navigate(`/details/update/${id}`)
+    navigate(`/details/update/${id}`)
   }
   //console.log(id)
   return (
@@ -53,53 +53,53 @@ const Details = () => {
       <div>
         <NavBar />
       </div>
-                    
-      { 
-        Object.keys(detail).length > 0 && !loading ?  (
-            <div>
-              <Buy/>
-          <div className={s.botoness}>
-          <button className={s.btn} onClick={(e) => handleDeleteBook(e)}>DELETE</button>
-          <button className={s.btn} onClick={(e) => handleUpdateBook(e)}>UPDATE</button>
-          </div>
-          <div className={s.background}>
-          
-          
-            <div className={s.name}>
-            
-            <h3 className={s.pName}>{detail.nombre}</h3>
-            <img  src={detail.image} alt="not found" className={s.image}
-            onError={(e)=>e.target.setAttribute('src','https://pbs.twimg.com/profile_images/1611903252/Books-Icon120x120_400x400.jpg')} />
-            <h3 className={s.pName}>Precio: {'$'+detail.price+'.00'}</h3>
-            <button className={s.btnn}>Añadir a Carrito</button>
-              
+
+      {
+        Object.keys(detail).length > 0 && !loading ? (
+          <div>
+            {/* <Buy /> */}
+            <div className={s.botoness}>
+              <button className={s.btn} onClick={(e) => handleDeleteBook(e)}>DELETE</button>
+              <button className={s.btn} onClick={(e) => handleUpdateBook(e)}>UPDATE</button>
             </div>
-            <div className={s.description}>
-              <h5 className={s.h5}>Colección</h5>
-              {detail.colection}
+            <div className={s.background}>
 
-              <h5 className={s.h5}>Categoría</h5>
-              {detail.category}
 
-              <h5 className={s.h5}>Ranking</h5>
-              {detail.ranking ? detail.ranking : "no tiene ranking"}
-          
-              <h5 className={s.h5}>Descripción</h5>
-              {detail.descripcion}
+              <div className={s.name}>
+
+                <h3 className={s.pName}>{detail.nombre}</h3>
+                <img src={detail.image} alt="not found" className={s.image}
+                  onError={(e) => e.target.setAttribute('src', 'https://pbs.twimg.com/profile_images/1611903252/Books-Icon120x120_400x400.jpg')} />
+                <h3 className={s.pName}>Precio: {'$' + detail.price + '.00'}</h3>
+                <button className={s.btnn}>Añadir a Carrito</button>
+
+              </div>
+              <div className={s.description}>
+                <h5 className={s.h5}>Colección</h5>
+                {detail.colection}
+
+                <h5 className={s.h5}>Categoría</h5>
+                {detail.category}
+
+                <h5 className={s.h5}>Ranking</h5>
+                {detail.ranking ? detail.ranking : "no tiene ranking"}
+
+                <h5 className={s.h5}>Descripción</h5>
+                {detail.descripcion}
+              </div>
+              <Link to='/checkout'>
+                <button onClick={(e) => handleBuy(e)} >BUY NOW</button>
+              </Link>
             </div>
-            <Link to= '/checkout'>
-            <button onClick={(e) => handleBuy(e)} >BUY NOW</button>
-            </Link>
-            </div> 
 
           </div>
-        )  :  !Object.keys(detail).length > 0 && loading ? (
-            <Loading/>
-        ): detail.length === 0 && (
-            <NotFoundGral/>
-         )
-        }
-          
+        ) : !Object.keys(detail).length > 0 && loading ? (
+          <Loading />
+        ) : detail.length === 0 && (
+          <NotFoundGral />
+        )
+      }
+
     </div>
   )
 }
