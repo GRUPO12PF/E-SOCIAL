5a1bc70c78bd3e804f4bd620fe6ebb89995bc8ce
Nekrocow <nekrocow@gmail.com>
2022-06-16 13:00:11 -0300

    TOMAAAAA!!!

--- frontend/src/components/CheckOut/CheckOut.jsx
+++ frontend/src/components/CheckOut/CheckOut.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from "react-router";
-import { useParams } from 'react-router-dom'
-import { buyBook } from '../../redux/actions/actionBuy'
+import React from "react"
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+import Checkoutform from "./CheckoutForm/CheckoutForm"
 
-const checkOut = () => {
-    useEffect(() => {
-    
-        dispatch(buyBook(id))
-            
-    }, [dispatch])
+// import { buyBook } from '../../redux/actions/actionBuy'
 
-    return(
-        <div>
-        </div>
-    )
+const stripePromise = loadStripe('pk_test_51LAg9hHDqRgCh1WMinPYGhVaxGp2m9Df26h0orDwuaSZJak0vVZl2rUcrvlbK14XVmWZ9kScsTCXMWiZLFA6Abek00ezDxZtwN') // TODO cambiar por la variable con la secret key!
+
+const CheckOut = () => {
+  // useEffect(() => {
+  //     dispatch(buyBook(id))
+  // }, [dispatch])
+
+  return (
+    <Elements stripe={stripePromise}>
+      <div>
+        <Checkoutform />
+      </div>
+    </Elements>
+  )
 }
 
-export default checkOut;
+export default CheckOut
---
+++ frontend/src/components/CheckOut/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+import { useSelector } from 'react-redux'
+import s from './CheckoutForm.module.css'
+
+import {
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js"
+
+import axios from "axios"
+
+const CheckoutForm = () => {
+  const stripe = useStripe()
+  const elements = useElements()
+  const id = useSelector(state => state.detail._id)
+  console.log("🚀 ~ file: CheckoutForm.jsx ~ line 14 ~ id", id)
+
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    })
+    setLoading(true)
+
+    if (!error) {
+      // console.log(paymentMethod)
+      const { id } = paymentMethod
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            amount: 10000, //cents
+          }
+        )
+        console.log(data)
+
+        elements.getElement(CardElement).clear()
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+  }
+
+  console.log(!stripe || loading)
+
+  return (
+    <div className={s.bigDiv}>
+      <form className={s.form} onSubmit={handleSubmit}>
+        {/* Product Information */}
+        <img
+          src="https://i.imgur.com/YoEVHEg.jpg" // TODO cambiar por imagen del libro
+          alt="asfasgf" // TODO cambiar por texto acorde
+          className={s.productImg}
+        />
+
+        <h3 className="text-center my-2">Price: 100$</h3> {/* TODO cambiar por price del libro */}
+
+        {/* User Card Input */}
+        <div className={s.cardElement}>
+          <CardElement />
+        </div>
+
+        <button disabled={!stripe} className={s.butones}>
+          {loading ? (
+            <div className="spinner-border text-light" role="status">
+              <span className="sr-only">Cargando...</span>
+            </div>
+          ) : (
+            "Comprar"
+          )}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CheckoutForm
