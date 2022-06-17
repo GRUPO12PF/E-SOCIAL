e568de51d21defd388faf56915ee4ba645e7aded
Maria Lucia Bidal <maluciabidal@gmail.com>
2022-06-17 20:39:22 -0300

    footer

--- frontend/src/components/CommonComponents/Home/Home.jsx
+++ frontend/src/components/CommonComponents/Home/Home.jsx
@@ -2,32 +2,15 @@ import React from 'react'
 import AllBooks from '../AllBooks/AllBooks.jsx'
 import NavBar from '../NavBar/NavBar'
 import SearchBar from '../SearchBar/SearchBar.jsx'
-<<<<<<< HEAD
-import Footer from '../Footer/Footer.jsx' 
-
-=======
 import Alien from '../../../Iconos/Alien.jsx'
->>>>>>> db1b2da8cef187a0889a917b29dffdbb820a8f00
-
+import Footer from '../Footer/Footer.jsx'
 export default function Home() {
 return(
   <div className="contenedorGral">
     <NavBar />
-    <div> 
       <SearchBar/>
-    </div>
-    <div> 
       <AllBooks/>
-<<<<<<< HEAD
-       </div>
-<div>
-  <Footer/>
-</div>
-=======
-    </div>
-    <Alien/>
->>>>>>> db1b2da8cef187a0889a917b29dffdbb820a8f00
+    <Footer/>
   </div>
 )
 }
-
