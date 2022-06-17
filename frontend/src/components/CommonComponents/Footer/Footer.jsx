c4efd529c485c6226ebf31bbc26aa35bd9256d44
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-17 18:58:36 -0500

    cambio nombre en el footer

--- frontend/src/components/CommonComponents/Footer/Footer.jsx
+++ frontend/src/components/CommonComponents/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <hr />
         <div className="row">
           <p className="col-sm">
-            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
+            &copy;{new Date().getFullYear()} E-SOCIAL INC | All rights reserved |
             Terms Of Service | Privacy
           </p>
         </div>
