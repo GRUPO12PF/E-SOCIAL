d0b527ecbcb88820bb62993dbf6aeadc30877a6f
CEECISOOL <cbritez6@gmail.com>
2022-06-18 03:48:12 -0300

    historial de ordenes y libros creados

--- frontend/src/components/CommonComponents/Profile/Profile.jsx
+++ frontend/src/components/CommonComponents/Profile/Profile.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router";
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 
  
 
 function Profile() {
-  const dispatch = useDispatch()
   const navigate = useNavigate();
   const user = useSelector(state => state.usuario)
   const idUser = user._id
   console.log(idUser)
   
   
-  function handleOnClick() {      
+  function handleOnClickHistory() {      
     navigate(`/historyOrders/${idUser}`);
-
+  }
+  function handleOnClickBooks() {      
+    navigate(`/bookCreated/${idUser}`);
   }
 
   return (
     <div>
-      <button onClick={e => (handleOnClick(e))}>History Orders</button>
+      <button onClick={e => (handleOnClickHistory(e))}>History Orders</button>
+      <button onClick={e => (handleOnClickBooks(e))}>Books Created</button>
     </div>
     
   )
---
+++ frontend/src/redux/actions/actionCreatedUser.js
@@ -0,0 +1,23 @@
+import clienteAxios from "../../config/clienteAxios";
+
+export function usuarioCreated(payload) {
+    return async function (dispatch) {
+      const id = localStorage.getItem("token");
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${id}`,
+        },
+      };
+      try {
+        const json = await clienteAxios.get(`/usuarioCreador/${payload}`, config);
+        console.log(json.data)
+        return dispatch({
+          type: "USUARIO_CREATED",
+          payload: json.data,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }
