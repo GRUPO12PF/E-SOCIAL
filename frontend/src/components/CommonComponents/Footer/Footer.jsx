a37716c041385c1918c95dc7ea773c7f41fef29d
Maria Lucia Bidal <maluciabidal@gmail.com>
2022-06-20 13:49:30 -0300

    footer

--- frontend/src/components/CommonComponents/Footer/Footer.jsx
+++ frontend/src/components/CommonComponents/Footer/Footer.jsx
@@ -4,17 +4,14 @@ import GitHub from '../../../Iconos/GitHub';
 function Footer() {
   return (
           <div className={s.footer}>
-        <div>
-        <a href='https://github.com/GRUPO12PF/E-SOCIAL' >
+           <a href='https://github.com/GRUPO12PF/E-SOCIAL' >
         <GitHub/>
         </a>
-        </div>
-          <div className={s.leyenda}>
-          <p className="col-sm">
+          <p className={s.footerDos} >
             &copy;{new Date().getFullYear()} E-SOCIAL INC | All rights reserved |
             Terms Of Service | Privacy
+        
             </p>
-          </div>
               </div>
   );
 }
