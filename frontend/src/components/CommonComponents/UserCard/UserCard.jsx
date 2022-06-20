3d3ab41f9cb051ce4d75a513957290a4ed89ea04
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-19 22:34:45 -0500

    creacion nuevo componente vendedor

---
+++ frontend/src/components/CommonComponents/UserCard/UserCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import Footer from '../Footer/Footer'
+
+function UserCard() {
+    return (
+        <div>
+            <NavBar />
+            SELLER PROFILE
+            <Footer />
+        </div>
+
+    )
+}
+
+export default UserCard
