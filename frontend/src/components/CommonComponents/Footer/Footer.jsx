72ee01712bc36a9e09d6af32c667db8a08792cef
Maria Lucia Bidal <maluciabidal@gmail.com>
2022-06-20 10:33:19 -0300

    footer

--- frontend/src/components/CommonComponents/Footer/Footer.jsx
+++ frontend/src/components/CommonComponents/Footer/Footer.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import s from './Footer.module.css'
 import GitHub from '../../../Iconos/GitHub';
 function Footer() {
   return (
           <div className={s.footer}>
         <div>
-        <Link to='https://github.com/GRUPO12PF/E-SOCIAL' >
-        
+        <a href='https://github.com/GRUPO12PF/E-SOCIAL' >
         <GitHub/>
-        </Link>
+        </a>
         </div>
-        
           <div className={s.leyenda}>
           <p className="col-sm">
             &copy;{new Date().getFullYear()} E-SOCIAL INC | All rights reserved |
