1066c0aab8b42ec34a9f006799c7f857edb6e999
Nekrocow <nekrocow@gmail.com>
2022-06-21 20:35:47 -0300

    mejoras form y algo de SearchBar

--- frontend/src/components/UserRegisteredComponents/Form/Forms.jsx
+++ frontend/src/components/UserRegisteredComponents/Form/Forms.jsx
@@ -27,10 +27,17 @@ const Forms = () => {
           initialValues={{
             nombre: '',
             autor: '',
+            idioma: '',
+            editorial: '',
+            edicion: '',
+            tapa: '',
+            año_de_pub: '',
+            cant_pags: '',
             descripcion: '',
             price: '',
             image: '',
             colection: '',
+            ilustrado: false,
             category: []
           }}
 
@@ -41,8 +48,42 @@ const Forms = () => {
               errors.nombre = 'Campo requerido.'
             } else if (!/^\S.*$/.test(values.nombre)) {
               errors.nombre = 'El primer caracter no puede ser un espacio'
-            } else if (!/^(\d|[a-z]|[,.:;¡!¿?']|[À-ÿ]|\s){1,40}$/i.test(values.nombre)) {
-              errors.nombre = 'Debe ingresar un nombre de hasta 40 caracteres.'
+            } else if (!/^(\d|[a-z]|[\u00f1\u00d1]|[,.:;¡!¿?']|[À-ÿ]|\s){1,40}$/i.test(values.nombre)) {
+              errors.nombre = 'Ingrese un nombre válido de hasta 40 caracteres.'
+            }
+
+            if (!values.autor) {
+              errors.autor = 'Campo requerido.'
+            } else if (!/^\S.*$/.test(values.autor)) {
+              errors.autor = 'El primer caracter no puede ser un espacio'
+            } else if (!/^(|[a-z]|[()']|[À-ÿ]|[\u00f1\u00d1]|\s){1,40}$/i.test(values.autor)) {
+              errors.autor = 'Ingrese un autor válido de hasta 40 caracteres.'
+            }
+
+            if (!values.idioma) {
+              errors.idioma = 'Campo requerido.'
+            } else if (!/^\S.*$/.test(values.idioma)) {
+              errors.idioma = 'El primer caracter no puede ser un espacio'
+            } else if (!/^([a-z]|[\u00f1\u00d1]|\s){1,20}$/i.test(values.idioma)) {
+              errors.idioma = 'Ingrese un idioma válido de hasta 40 caracteres.'
+            }
+
+            if (!/^\S.*$/.test(values.editorial)) {
+              errors.editorial = 'El primer caracter no puede ser un espacio'
+            } else if (!/^(\d|[a-z]|[\u00f1\u00d1]|[,.:;¡!¿?']|[À-ÿ]|\s){1,40}$/i.test(values.editorial)) {
+              errors.editorial = 'Ingrese un nombre de editorial de hasta 40 caracteres.'
+            }
+
+            if (/(\D)/.test(values.edicion) || values.edicion < 1 && values.edicion.toString().length > 0) {
+              errors.edicion = 'Ingrese un Nº de edición mayor a 0.'
+            }
+
+            if (values.año_de_pub && !/^[012][0-9]{0,3}$/.test(values.año_de_pub)) {
+              errors.año_de_pub = 'Ingrese un año en formato AAAA.'
+            }
+
+            if (/(\D)/.test(values.cant_pags) || values.cant_pags < 1) {
+              errors.cant_pags = 'Ingrese un número de págs. válido.'
             }
 
             if (values.descripcion.length < 6) {
@@ -51,14 +92,10 @@ const Forms = () => {
               errors.descripcion = 'La descripción debe contar con un máximo de 1500 caracteres.'
             }
 
-            if (!values.price) {
-              errors.price = 'Campo requerido.'
-            } else if (/(\D)/.test(values.price)) {
+            if (/(\D)/.test(values.price)) {
               errors.price = 'Ingrese el precio en centavos de USD.'
-            }
-
-            if (!values.colection) {
-              errors.colection = 'Campo requerido.'
+            } else if (!values.price || values.price < 50) {
+              errors.price = 'Ingrese un precio válido.'
             }
 
             if (!values.category) {
@@ -86,20 +123,98 @@ const Forms = () => {
         >
           {({ errors, handleSubmit, values, category }) => (<Form action="" onSubmit={handleSubmit} className={s.formik} >
             <div className={s.form}>
-              
-              <label htmlFor="" className={s.label} >Nombre</label>
+
+              <label htmlFor="" className={s.label} >Nombre*</label>
               <div>
                 <Field
                   className={s.input}
                   type="text"
                   name="nombre"
                   id="nombre"
-                  placeholder="nombre..."
                 />
                 <ErrorMessage name='nombre' component={() => (<p>{errors.nombre}</p>)} />
               </div>
 
-              <label htmlFor="" className={s.label} >Colección</label>
+              <label htmlFor="" className={s.label} >Autor*</label>
+              <div>
+                <Field
+                  className={s.input}
+                  type="text"
+                  name="autor"
+                  id="autor"
+                />
+                <ErrorMessage name='autor' component={() => (<p>{errors.autor}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Idioma*</label>
+              <div>
+                <Field
+                  className={s.textarea}
+                  type="text"
+                  name="idioma"
+                  id="idioma"
+                />
+                <ErrorMessage name='idioma' component={() => (<p>{errors.idioma}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Editorial</label>
+              <div>
+                <Field
+                  className={s.textarea}
+                  type="text"
+                  name="editorial"
+                  id="editorial"
+                />
+                <ErrorMessage name='editorial' component={() => (<p>{errors.editorial}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Edición</label>
+              <div>
+                <Field
+                  className={s.input}
+                  type="number"
+                  name="edicion"
+                  id="edicion"
+                />
+                <ErrorMessage name='edicion' component={() => (<p >{errors.edicion}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Tipo de tapa</label>
+              <div>
+                <Field
+                  className={s.textarea}
+                  type="text"
+                  name="tapa"
+                  id="tapa"
+                  placeholder="Blanda, Dura..."
+                />
+                <ErrorMessage name='tapa' component={() => (<p>{errors.tapa}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Año de publicación</label>
+              <div>
+                <Field
+                  className={s.input}
+                  type="number"
+                  name="año_de_pub"
+                  id="año_de_pub"
+                  placeholder="AAAA..."
+                />
+                <ErrorMessage name='año_de_pub' component={() => (<p >{errors.año_de_pub}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Cantidad de páginas</label>
+              <div>
+                <Field
+                  className={s.input}
+                  type="number"
+                  name="cant_pags"
+                  id="cant_pags"
+                />
+                <ErrorMessage name='cant_pags' component={() => (<p >{errors.cant_pags}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Saga / Serie</label>
               <div>
                 <Field
                   className={s.input}
@@ -110,7 +225,18 @@ const Forms = () => {
                 <ErrorMessage name='colection' component={() => (<p>{errors.colection}</p>)} />
               </div>
 
-              <label htmlFor="" className={s.label} >Precio</label>
+              <label htmlFor="" className={s.label} >Fotografías del ejemplar</label> {/* ToDo: acá hay que cambiar por upload en Cloudinary */}
+              <div>
+                <Field
+                  className={s.input}
+                  type="text"
+                  name="image"
+                  id="image"
+                />
+                <ErrorMessage name='image' component={() => (<p>{errors.image}</p>)} />
+              </div>
+
+              <label htmlFor="" className={s.label} >Precio*</label>
               <div>
                 <Field
                   className={s.input}
@@ -122,18 +248,7 @@ const Forms = () => {
                 <ErrorMessage name='price' component={() => (<p >{errors.price}</p>)} />
               </div>
 
-              <label htmlFor="" className={s.label} >Imagen</label>
-              <div>
-                <Field
-                  className={s.input}
-                  type="text"
-                  name="image"
-                  id="image"
-                />
-                <ErrorMessage name='image' component={() => (<p>{errors.image}</p>)} />
-              </div>
-
-              <label htmlFor="" className={s.label} >Descripción</label>
+              <label htmlFor="" className={s.label} >Descripción*</label>
               <div>
                 <Field
                   className={s.textarea}
@@ -145,16 +260,34 @@ const Forms = () => {
                 <ErrorMessage name='descripcion' component={() => (<p>{errors.descripcion}</p>)} />
               </div>
 
-              <label htmlFor="" className={s.label}>Categorías</label>
+              <label htmlFor="" className={s.label}>Ilustrado</label>
+              <div>
+                <Field
+                  className={s.textarea}
+                  as="select"
+                  id="ilustrado"
+                  name="ilustrado"
+                >
+                  <option value={false}>NO</option>
+                  <option value={true}>SI</option>
+                </Field>
+              </div>
+
+              <label htmlFor="" className={s.label}>Categorías*</label>
               <div className={s.chek}>
                 <div role="group" aria-labelledby="checkbox-group" >
                   {categories?.map((e, i) =>
-                    <label> <Field type="checkbox" name="category" value={`${e}`} key={i} /> {e} </label>
+                    <div key={i} > <Field type="checkbox" name="category" value={`${e}`} /> {e} </div>
                   )}
+                  {/* <ErrorMessage name='category' component={() => (<p>{errors.category}</p>)} />      NO ESTÁ FUNCANDO! */}
                 </div>
               </div>
 
-              <button className={s.sendMsg} type="submit">ENVIAR</button>
+              <button
+                className={s.sendMsg}
+                type="submit"
+                disabled={errors.nombre || errors.autor || errors.idioma || errors.price || errors.category || errors.descripcion}
+              >ENVIAR</button>
             </div>
           </Form>)}
         </Formik>
--- frontend/src/utils/helperFunctions.js
+++ frontend/src/utils/helperFunctions.js
@@ -4,6 +4,16 @@ export function formatToCurrency(value) {
   return finalPrice
 }
 
+// export function validateTextName(name) { // tampoco lo pude usar
+//   if (!values[name]) {
+//     return errors[name] = 'Campo requerido.'
+//   } else if (!/^\S.*$/.test(values[name])) {
+//     return errors[name] = 'El primer caracter no puede ser un espacio'
+//   } else if (!/^(|[a-z]|[,.:;¡!¿?']|[À-ÿ]|\s){1,40}$/i.test(values[name])) {
+//     return errors[name] = 'No puede superar los 40 caracteres.'
+//   }
+// }
+
 // export function listedArray(value) { // no sé por qué no funca jaja
 //   value?.sort((a, b) => a.localeCompare(b)).join(', ')
 // }
