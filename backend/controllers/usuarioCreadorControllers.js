91cfb33fe3b51c7f8710084e68d68589d376e7a2
Cecilia Acevedo <79501511+CEECISOOL@users.noreply.github.com>
2022-06-22 19:29:26 -0300

    Update usuarioCreadorControllers.js

--- backend/controllers/usuarioCreadorControllers.js
+++ backend/controllers/usuarioCreadorControllers.js
@@ -4,7 +4,7 @@ import Book from '../models/Book.js'
 const obtenerLibrosUsuarios = async (req, res) => {
   try {
     const { id } = req.params
-    const booksUsuarios = await Book.find({ creador: id })
+    const booksUsuarios = await Book.find({ creador: id }).populate("creador")
     let response = booksUsuarios
     
     res.send(response)
