a22beadc2cc101d2be547d81fea827d0a8664870
Nekrocow <nekrocow@gmail.com>
2022-06-22 16:53:18 -0300

    probando logica

---
+++ backend/controllers/imageController.js
@@ -0,0 +1,35 @@
+import { uploadImage } from "../libs/cloudinary.js"
+import fs from "fs-extra"
+
+const postBookImage = async (req, res) => {
+  // const formatos = ["png", "jpg", "webp", "gif"]
+  // if (
+  //   !formatos.includes(
+  //     req.files.image.name.split(".")[
+  //     req.files.image.name.split(".").length - 1
+  //     ]
+  //   )
+  // ) {
+  //   return res.status(400).send({ msg: "Invalid image format (jpg, png, webp or gif)" })
+  // }
+
+  try {
+    if (req.files.image) {
+      const response = await uploadImage(req.files.image.tempFilePath)
+      await fs.remove(req.files.image.tempFilePath)
+
+      const image = {
+        url: response.secure_url,
+        public_id: response.public_id,
+      }
+
+      res.json(image)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {
+  postBookImage
+}
