459aa18297fef90ce61c81a73c2f7c3bea652f9f
CEECISOOL <cbritez6@gmail.com>
2022-06-23 23:31:33 -0300

    en el perfil del vendedor aparecen los libros vendidos

--- frontend/src/components/CommonComponents/ProfileUsers/ProfileBooks.jsx
+++ frontend/src/components/CommonComponents/ProfileUsers/ProfileBooks.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import s from '../ProfileUsers/ProfileBook.module.css'
 
-function ProfileBook({ nombre, autor, image }) {
+function ProfileBook({ nombre, autor, image, order }) {
+
+    let vendido
+    if (order.length > 0) {
+        vendido = 'VENDIDO'
+    }
 
     return (
-
-        
         <div className="container-profile">
             <div className={s.card}>
                 <div className={s.cardbody}>
+                    <div className={s.vendido}>{vendido ? vendido : null}</div>
                     <p className={s.texto}>{nombre}</p>
                     <p className={s.texto}>{autor}</p>
                     <img className={s.im} src={image} />
