b710d94316764906bbc8417720addcb4543ae910
Nekrocow <nekrocow@gmail.com>
2022-06-23 21:49:37 -0300

    todoEsp CON galpón

---
+++ frontend/src/components/UserRegisteredComponents/Answers/Answers.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { allAnswers  } from "../../../redux/actions/actionQA";
+import { useEffect } from "react";
+import NavBar from "../../CommonComponents/NavBar/NavBar";
+import AnswerDetail from "./AnswerDetail";
+
+export default function Questions(){
+    const dispatch = useDispatch()
+    const { id } = useParams();
+    const answers = useSelector((state) => state.answers)
+    const answersUser = answers.filter(ele => ele.idVendedor._id === id)  
+
+    console.log("a ver qué trae", answersUser)
+    
+    
+    useEffect(() => {   
+         dispatch(allAnswers(id))
+    }, [dispatch]);
+    
+    return(
+        <div>
+          <NavBar />
+          <h3>HISTORIAL DE RESPUESTAS</h3>
+            {answersUser?.map((e, i)=>{
+                 return (
+                    <div _id= {e._id} key={i}>
+                          <AnswerDetail
+                            mensaje= {e.mensaje}
+                            book={e.book}
+                          />
+                    </div>
+                  );
+                })}
+        </div>
+    )
+}
