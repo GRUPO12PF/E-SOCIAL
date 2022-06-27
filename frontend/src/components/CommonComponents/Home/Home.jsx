cc1d41edcfa58531f05cad0a2d4b9f7e68fcdef8
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-27 15:52:42 -0500

    notfoundnuevo

--- frontend/src/components/CommonComponents/Home/Home.jsx
+++ frontend/src/components/CommonComponents/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <div className="contenedorTotal">
         <NavBar />
-        <Carousell />
+                <Carousell />
         <AllBooks />
       </div>
 
