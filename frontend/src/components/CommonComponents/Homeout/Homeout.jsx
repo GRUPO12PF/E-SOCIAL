444e1cf09fb0815a481c780650c9b16ee43212b4
CEECISOOL <cbritez6@gmail.com>
2022-06-27 14:44:13 -0300

    login y register

--- frontend/src/components/CommonComponents/Homeout/Homeout.jsx
+++ frontend/src/components/CommonComponents/Homeout/Homeout.jsx
@@ -1,43 +1,78 @@
 import React, { useEffect } from "react"
 import { useNavigate } from "react-router"
-import { useDispatch } from "react-redux"
-import Modal from "react-modal"
-import Login from '../Login/Login'
-import Register from "../Register/Register"
-import { autenticarUser } from "../../../redux/actions/actionUser.js"
-import s from "./Homeout.module.css"
+import { registroGoogle } from "../../../redux/actions/actionUser"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { GoogleLogin } from "@react-oauth/google"
+import { useDispatch, useSelector } from "react-redux"
+import './style.css'
+import Login from "./Register"
+import Register from "./Login"
 
-Modal.setAppElement("#root")
 
 export default function Homeout() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+ const dispatch = useDispatch();
+ const navigate = useNavigate()
+ const token = localStorage.getItem("token")
 
-  useEffect(() => {
-    const token = localStorage.getItem("token")
-    if (!token) {
-      return
-    }
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }
-    let usuarioA = dispatch(autenticarUser(config))
-    usuarioA ? navigate("/") : null
-  }, [])
-  
+ useEffect(() => {
+
+  token ? navigate("/") : null
+
+}, [token])
+
+  function responseGoogle(el) {
+    dispatch(registroGoogle(el))
+
+    setTimeout(function () {
+      window.location.reload(1)
+    }, 1500) // After 2,5 secs
+  }
+ 
+  //let container = document?.getElementById('container');
+
+  function click() {
+    (document?.getElementById('container')).classList.add("right-panel-active");
+  };
+
+  function clickk() {
+    ( document?.getElementById('container')).classList.remove("right-panel-active");
+  };
+
   return (
-    <div className={s.container}>
-      <div className={s.login}>
-        <h2>INGRESAR</h2>
-        <Login />
+    <div>
+
+      <div>
+        <div class="container" id="container" >
+          <div class="form-container sign-up-container">
+            <Login/>
+            </div> 
+          <div class="form-container sign-in-container">
+            <Register/>
+          </div>
+          <div class="overlay-container">
+            <div class="overlay">
+              <div class="overlay-panel overlay-left">
+                <h1>Hola nuevamente!</h1>
+                <p>Para volver a iniciar sesion ingrese su informacion personal</p>
+                <button class="ghost" onClick={e=>clickk(e)} id="signIn">Ingresar</button>
+              </div>
+              <div class="overlay-panel overlay-right">
+                <h1>Hola le damos la bienvenida!</h1>
+                <p>Ingrese sus datos personales y comience a comprar libros unicos</p>
+                <button class="ghost" onClick={e=>click(e)} id="signUp">Registrarse</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={s.register}>
-        <h2>REGISTRATE</h2>
-        <Register />
-      </div>
+      <div className="logg">
+                  <GoogleOAuthProvider
+                    clientId={`${import.meta.env.VITE_URL_CLIENT_ID}`}
+                  >
+                    <GoogleLogin login_uri="" onSuccess={responseGoogle} />
+
+                  </GoogleOAuthProvider>
+                </div>
     </div>
   )
 }
