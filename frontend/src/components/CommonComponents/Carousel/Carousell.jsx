18f94a89f56e86109726bb9a64097bf2e63b0f9c
Caro Tobar J <106922571+GRUPO12PF@users.noreply.github.com>
2022-06-27 20:38:27 -0500

    imagenes nuevas carrusel

--- frontend/src/components/CommonComponents/Carousel/Carousell.jsx
+++ frontend/src/components/CommonComponents/Carousel/Carousell.jsx
@@ -1,7 +1,16 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
-import banner1 from "../../../assets/images/Banners1.jpg"
-import banner2 from "../../../assets/images/Banners2.jpg"
+import ban01 from "../../../assets/images/ban01.png"
+import ban02 from "../../../assets/images/ban02.png"
+import ban03 from "../../../assets/images/ban03.png"
+import ban04 from "../../../assets/images/ban04.png"
+import ban05 from "../../../assets/images/ban05.png"
+import ban06 from "../../../assets/images/ban06.png"
+import ban07 from "../../../assets/images/ban07.png"
+import ban08 from "../../../assets/images/ban08.png"
+import ban09 from "../../../assets/images/ban09.png"
+import ban10 from "../../../assets/images/ban10.png"
+import ban11 from "../../../assets/images/ban11.png"
 
 const Carousell = () => {
 
@@ -11,18 +20,39 @@ const Carousell = () => {
     <div className="slider">
     <div className="slide-track">
       <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+        <img src={ban01} height="200" width="250" alt="" />
       </div>
       <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+        <img src={ban02} height="200" width="250" alt="" />
       </div>
       <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+        <img src={ban03} height="200" width="250" alt="" />
       </div>
       <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+        <img src={ban04} height="200" width="250" alt="" />
       </div>
       <div className="slide">
+        <img src={ban05} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban06} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban07} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban08} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban09} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban10} height="200" width="250" alt="" />
+      </div>
+      <div className="slide">
+        <img src={ban11} height="200" width="250" alt="" />
+      </div>
+      {/* <div className="slide">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
       </div>
       <div className="slide">
@@ -30,28 +60,7 @@ const Carousell = () => {
       </div>
       <div className="slide">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-      </div>
-      <div className="slide">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-      </div>
+      </div> */}
     </div>
   </div>
     // <Carousel>
