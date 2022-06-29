c251dd96d8747332a4639d24b3f51a0fad135662
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2022-06-28 23:58:22 -0300

    algunas alerts

--- frontend/src/components/UserRegisteredComponents/Review/Review.jsx
+++ frontend/src/components/UserRegisteredComponents/Review/Review.jsx
@@ -62,16 +62,10 @@ const Review = ({id, closeModal}) => {
       console.log("acá están todos los inputs", input)
       if(input.score > 5 || input.score <= 1){
         swal({
-<<<<<<< HEAD
           title: "¡El score debe ser entre 1 y 5!",
           text: " ",
           button: "Ok!",
         })
-=======
-          title: "El score debe ser entre 1 y 5!",
-        });
-
->>>>>>> 2061b45283d17c87e4753903c3b7e1d4e4786f9d
       } else {
         dispatch(review(input))
         swal({
@@ -88,18 +82,10 @@ const Review = ({id, closeModal}) => {
         })
         Navigate("/")
       }
-<<<<<<< HEAD
     } else {
       swal({
         title: "Faltan campos por completar",
       });
-=======
-    }else{
-    swal({
-      title: "Faltan campos por completar",
-    });
-     
->>>>>>> 2061b45283d17c87e4753903c3b7e1d4e4786f9d
     }
   }
 
